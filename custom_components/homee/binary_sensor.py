f2653f7efb248f6a2bd36524ceb6566dcaace42e
Taraman17 <mozilla@adrario.de>
2023-10-26 12:06:13 +0000

    Add Battery_low & link_quality sensors

--- custom_components/homee/binary_sensor.py
+++ custom_components/homee/binary_sensor.py
@@ -17,6 +17,7 @@ from .const import CONF_DOOR_GROUPS, CONF_GROUPS, CONF_WINDOW_GROUPS
 _LOGGER = logging.getLogger(__name__)
 
 HOMEE_BINARY_SENSOR_ATTRIBUTES = [
+    AttributeType.BATTERY_LOW_ALARM,
     AttributeType.HIGH_TEMPERATURE_ALARM,
     AttributeType.LOCK_STATE,
     AttributeType.ON_OFF,
@@ -30,6 +31,16 @@ def get_device_class(attribute: HomeeAttribute) -> int:
     device_class = BinarySensorDeviceClass.OPENING
     state_attr = AttributeType.OPEN_CLOSE
     translation_key = "opening_sensor"
+
+    if attribute.type == AttributeType.BATTERY_LOW_ALARM:
+        state_attr = AttributeType.BATTERY_LOW_ALARM
+        device_class = BinarySensorDeviceClass.BATTERY
+        translation_key = "battery_low_sensor"
+
+    if attribute.type == AttributeType.HIGH_TEMPERATURE_ALARM:
+        state_attr = AttributeType.HIGH_TEMPERATURE_ALARM
+        device_class = BinarySensorDeviceClass.HEAT
+        translation_key = "heat_sensor"
 
     if attribute.type == AttributeType.ON_OFF:
         state_attr = AttributeType.ON_OFF
@@ -46,11 +57,6 @@ def get_device_class(attribute: HomeeAttribute) -> int:
         device_class = BinarySensorDeviceClass.SMOKE
         translation_key = "smoke_sensor"
 
-    if attribute.type == AttributeType.HIGH_TEMPERATURE_ALARM:
-        state_attr = AttributeType.HIGH_TEMPERATURE_ALARM
-        device_class = BinarySensorDeviceClass.HEAT
-        translation_key = "heat_sensor"
-
     return (device_class, state_attr, translation_key)
 
 
@@ -61,7 +67,10 @@ async def async_setup_entry(hass: HomeAssistant, config_entry, async_add_devices
     for node in helpers.get_imported_nodes(hass, config_entry):
         for attribute in node.attributes:
             # Determine if the entity is a binary sensor.
-            if attribute.type in HOMEE_BINARY_SENSOR_ATTRIBUTES and not attribute.editable:
+            if (
+                attribute.type in HOMEE_BINARY_SENSOR_ATTRIBUTES
+                and not attribute.editable
+            ):
                 devices.append(HomeeBinarySensor(node, config_entry, attribute))
     if devices:
         async_add_devices(devices)
@@ -81,7 +90,7 @@ class HomeeBinarySensor(HomeeNodeEntity, BinarySensorEntity):
         self,
         node: HomeeNode,
         entry: ConfigEntry,
-        binary_sensor_attribute: HomeeAttribute = None
+        binary_sensor_attribute: HomeeAttribute = None,
     ) -> None:
         """Initialize a homee binary sensor entity."""
         HomeeNodeEntity.__init__(self, node, self, entry)
@@ -97,12 +106,13 @@ class HomeeBinarySensor(HomeeNodeEntity, BinarySensorEntity):
         (
             self._device_class,
             self._state_attr,
-            self._attr_translation_key
+            self._attr_translation_key,
         ) = get_device_class(self._on_off)
 
         # Set Window/Door device class based on configured groups
         if any(
-            str(group.id) in self._entry.options[CONF_GROUPS].get(CONF_WINDOW_GROUPS, [])
+            str(group.id)
+            in self._entry.options[CONF_GROUPS].get(CONF_WINDOW_GROUPS, [])
             for group in self._node.groups
         ):
             self._device_class = BinarySensorDeviceClass.WINDOW
